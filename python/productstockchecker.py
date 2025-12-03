cb8ade2b0ee1aa9cf447a9015c625a9762a1f770
jhansi205 <jhansi205@gmail.com>
2025-12-03 20:36:02 +0530

    add files

---
+++ python/productstockchecker.py
@@ -0,0 +1,8 @@
+user_input=int(input("Enter number of product units available: "))
+if (user_input ==0):
+    print("Out of Stock")
+elif (user_input >0 && user_input<=5 ):
+    print("Last few left!")
+    else:
+        print("Availble")
+    
